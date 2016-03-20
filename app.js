c96b446a294aae021d1233e40ac720770cf705d4
Juan Paucar <juantotish1@hotmail.com>
2016-03-20 16:24:23 -0500

    level 1 completed

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 (function(){
   var app = angular.module('store', [ ]);
   app.controller('StoreController', function() {
-    this.product = gem;
+    this.products = gems;
   });
 
-  var gem  = {
+  var gems  = [
+  {
     name: 'Dodecahedron',
     price: 2.95,
-    description: '...',
-  }
+    description: '. .  .',
+    canPurchase: true,
+  },
+  {
+    name: "Pentagonal Gem",
+    price: 5.95,
+    description: ". . . ",
+    canPurchase: false,
+  },
+  ]
 
 })();
